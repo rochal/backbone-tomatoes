23b9810d4819f7bf5fb424366deac59474d2f6cc
@rochal <piotr.rochala@gmail.com>
2013-01-13 21:23:11 +1100

    Remove unsused imports from app

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 define([
-  'router'
-], function(Router){
+  'router',
+  'collections/filmcollection'
+], function(Router, FilmCollection){
 
   var init = function(){
 
-  	// create application router
+    // create application router
     this.router = new Router();
   };
 
-  return { init: init};
+  return { init: init };
 });
